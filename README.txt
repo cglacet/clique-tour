073d555ea87463e4dbf2e4e0b698cb7253056fca
Christian Glacet <christian.glacet@gmail.com>
2018-04-04 11:51:41 +0200

    pip package test

---
+++ README.txt
@@ -0,0 +1,1 @@
+Build a clique tour for a given number of node n, if n is odd then the tour is eulerian otherwise the tour contains n/2 duplicate edges.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clique_tour',
+      version='0.2',
+      description='Build a tour of a clique giving a number of node.',
+      url='https://github.com/cglacet/clique-tour',
+      author='Christian Glacet',
+      author_email='christian.glacet@gmail.com',
+      license='MIT',
+      packages=['clique_tour'],
+      zip_safe=False)
